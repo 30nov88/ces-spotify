c782b09a4cc206a413b88fc05ee7910aa102d30a
pragaas <beppo.india@gmail.com>
2017-03-21 21:28:48 +0530

    integrated routing base

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 (function(){
     'use strict';
     angular
-    .module('spotApp',[])
-    .controller('MainCtrl',[fn_MainCtrl]);
+        .module('spotApp',['ngRoute'])
+        .controller('MainCtrl',[fn_MainCtrl])
+        .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+
+            $locationProvider.html5Mode({enabled: true, requireBase: false});
+            $routeProvider
+            .when("/", {
+                templateUrl : "views/home.html",
+                controller: "HomeCtrl"
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+        }]);
+    ;
+        
 
     function fn_MainCtrl(){
         var vm = this;
-        vm.test = "Assignment";
+        vm.heading = "Spotify API";
     }
 })();
 
